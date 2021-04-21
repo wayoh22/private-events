f79d3d048f33f8b92e953a2dedcc094247d52d1d
Ronnie <ronnie.tamburro@gmail.com>
2021-04-21 19:59:07 -0400

    Styled navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_115101) do
+ActiveRecord::Schema.define(version: 2021_04_21_230249) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

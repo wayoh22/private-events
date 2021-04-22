19cbfca2bd3be65b521c7295b084f947e575ce8a
Ronnie <ronnie.tamburro@gmail.com>
2021-04-21 20:07:47 -0400

    added migration for event title/location and made simple form

---
+++ db/migrate/20210422000458_add_event_fields_to_events.rb
@@ -0,0 +1,6 @@
+class AddEventFieldsToEvents < ActiveRecord::Migration[6.1]
+  def change
+    add_column :events, :title, :string
+    add_column :events, :location, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_230249) do
+ActiveRecord::Schema.define(version: 2021_04_22_000458) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "location"
   end
 
   create_table "users", force: :cascade do |t|
